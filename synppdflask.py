4d7c579735fd1daa1c71fdf5f32b2ae8a4397040
Darshika-Sonawane <124618965+Darshika-Sonawane@users.noreply.github.com>
2025-04-16 22:18:34 +0530

    Update synppdflask.py
    changed app route to '\'

--- synppdflask.py
+++ synppdflask.py
@@ -4,14 +4,13 @@ import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 xgb_classifier = joblib.load('xgb_classifier_model_syn.pkl')
 
-# Add this route to allow uptime checks
-@app.route('/', methods=['GET'])
-def health_check():
-    return 'Server is alive!', 200
+@app.route('/')
+def home():
+    return "Server is alive!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -19,15 +18,14 @@ def predict():
         data = request.get_json()
 
         input_df = pd.DataFrame([data])
-        input_df = input_df[['EPDS_Q1', 'EPDS_Q2', 'EPDS_Q3', 'EPDS_Q4', 'EPDS_Q5', 
+        input_df = input_df[['EPDS_Q1', 'EPDS_Q2', 'EPDS_Q3', 'EPDS_Q4', 'EPDS_Q5',
                              'EPDS_Q6', 'EPDS_Q7', 'EPDS_Q8', 'EPDS_Q9', 'EPDS_Q10']]
 
         prediction = xgb_classifier.predict(input_df)
-        predicted_label = int(prediction[0]) 
+        predicted_label = int(prediction[0])
         risk_status = "at risk" if predicted_label == 1 else "not at risk"
 
-        response = {'prediction': risk_status}
-        return jsonify(response)
+        return jsonify({'prediction': risk_status})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
