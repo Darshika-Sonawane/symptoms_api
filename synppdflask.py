ff34e68af397ea11f4fc055c2e37a9f15a68861a
Darshika-Sonawane <124618965+Darshika-Sonawane@users.noreply.github.com>
2025-04-16 22:11:36 +0530

    Update synppdflask.py
    added /get for uptimerobot

--- synppdflask.py
+++ synppdflask.py
@@ -8,19 +8,21 @@ CORS(app)
 
 xgb_classifier = joblib.load('xgb_classifier_model_syn.pkl')
 
+# Add this route to allow uptime checks
+@app.route('/', methods=['GET'])
+def health_check():
+    return 'Server is alive!', 200
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
 
         input_df = pd.DataFrame([data])
-
         input_df = input_df[['EPDS_Q1', 'EPDS_Q2', 'EPDS_Q3', 'EPDS_Q4', 'EPDS_Q5', 
                              'EPDS_Q6', 'EPDS_Q7', 'EPDS_Q8', 'EPDS_Q9', 'EPDS_Q10']]
 
-        # Make a prediction using the loaded model
         prediction = xgb_classifier.predict(input_df)
-
         predicted_label = int(prediction[0]) 
         risk_status = "at risk" if predicted_label == 1 else "not at risk"
 
